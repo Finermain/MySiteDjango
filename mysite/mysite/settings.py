ffe575ab1457e46ed5d3f49fd4ceb8f29ea2de82
Finermain <RedHood0878@yandex.ru>
2024-12-16 14:20:49 +0300

    Выполнена 2 часть туториала по Django.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',\
-    
+    'django.contrib.staticfiles',
+    'polls.apps.PollsConfig',
 ]
 
 MIDDLEWARE = [
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
